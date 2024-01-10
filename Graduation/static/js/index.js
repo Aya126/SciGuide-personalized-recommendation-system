bce4eebf1722367438b28c982a9190ea8a8f4d68
nadarabea1 <nadarabea81@gmail.com>
2024-01-10 20:43:35 +0200

    Upload whole project

---
+++ Graduation/static/js/index.js
@@ -0,0 +1,17 @@
+const contEl = document.querySelector(".cont");
+
+const btnEl = document.querySelector(".btn");
+
+const popupContEl = document.querySelector(".popup-cont");
+
+const closeIconEl = document.querySelector("#close-icon");
+
+btnEl.addEventListener("click", () => {
+  contEl.classList.add("active");
+  popupContEl.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", () => {
+  contEl.classList.remove("active");
+  popupContEl.classList.add("active");
+});
